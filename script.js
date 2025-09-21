6e1b76a86e6e9801f012b7ee6397d3860856b641
CHr15P4C <cspacitt@gmail.com>
2025-09-21 16:43:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//creating and logging 3 variables
+
+let studentName = "Christopher Pacitti";
+let studentAge = "28";
+let isEnrolled = true;
+
+console.log(studentName, studentAge, isEnrolled)
+
+
+//Demoing data types
+
+var aString = "A text value.";
+var anInt = 3;
+var aBoolean = false; 
+var anArray = ["A", "Collectin", "Of", "Values"];
+
+const studentInfo = new Object();
+studentInfo.name = studentName;
+studentInfo.age = studentAge;
+studentInfo.enrollment = isEnrolled;
+
+console.log("This is a string:", aString, "\nThis is an int:", anInt, "\nThis is a boolean:", aBoolean, "\nThis is an array:", anArray, "\nThis is an Object:", studentInfo);
+//Arithmetic Examples
+
+const a = 5;
+const b = 10;
+console.log("a =", a, "b =", b);
+
+var c = a + b;
+console.log("a+b=", c);
+c = b-a;
+console.log("b-a=", c);
+c=a*b;
+console.log("a*b=", c);
+c=b/a;
+console.log("b/a=", c);
+console.log("does a = b:", a==b);
+console.log("does a > b:", a>b);
+console.log("does a < b:", a<b);
+console.log("does a != b:", a!=b);
+//logical opperators
+const boolA = true
+const boolB = false
+console.log("boolA is=", boolA, "boolB is=", boolB);
+console.log("result of a&&b:", boolA&&boolB);
+console.log("result of a||b:", boolA||boolB);
+console.log("result of !a:", !boolA);
